904aead22639869308a25bbdc262b27815b559c4
Niqui <niqui@Niquis-MacBook-Air.local>
2017-05-04 07:03:58 -0700

    added views

--- ark_service/arkapp/views.py
+++ ark_service/arkapp/views.py
@@ -1,3 +1,25 @@
+import json
+ 
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
+ 
+from arkapp.models import Ark, Minter
 
-# Create your views here.
+def mint(request):
+	minter = Minter(template="template", prefix = "test-")
+	minter.save()
+	
+	return HttpResponse(json.dumps(minter.mint(10)))
+
+def bind(request):
+	minter = Minter(template="template", prefix = "test-")
+	minter.save()
+	ark = Ark()
+	ark.minter = minter
+	ark.key = minter.mint(1)[0]
+	ark.bind("test")
+	return HttpResponse(ark)
+	
+def arks(request):
+	arks = [str(ark.key) + "\n" for ark in Ark.objects.all()]
+	return HttpResponse(arks)
